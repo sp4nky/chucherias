a37ed110b6919f663029a2a74b040b24dbc47e23
d225364 <d225364@PC147127.corp.ute.com.uy>
2018-08-10 04:00:49 -0300

    Hibernate Funcional

--- src/main/java/com/chucherias/entidades/Producto.java
+++ src/main/java/com/chucherias/entidades/Producto.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
 @Entity
 @Table(name = "producto", schema = "CHUCHE")
 public class Producto implements Serializable{
